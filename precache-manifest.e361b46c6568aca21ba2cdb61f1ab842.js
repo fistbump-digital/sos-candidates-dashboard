f47d2d2ec9a59eabb4f73ff6de1ad40309ad1625
ShubhamFistbumpDigital <shubham.fistbump@gmail.com>
2020-11-19 21:47:48 +0530

    build

---
+++ precache-manifest.e361b46c6568aca21ba2cdb61f1ab842.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3205e4b9a3dac537c7c78879014e621",
+    "url": "/jobs-client/index.html"
+  },
+  {
+    "revision": "a682804bc797660da49c",
+    "url": "/jobs-client/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d6e46e2536cb5e5586fc",
+    "url": "/jobs-client/static/css/main.379300e0.chunk.css"
+  },
+  {
+    "revision": "a682804bc797660da49c",
+    "url": "/jobs-client/static/js/2.cb5e4c4c.chunk.js"
+  },
+  {
+    "revision": "737a875f63054be206d1a30f820ffc03",
+    "url": "/jobs-client/static/js/2.cb5e4c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e46e2536cb5e5586fc",
+    "url": "/jobs-client/static/js/main.a645d7d1.chunk.js"
+  },
+  {
+    "revision": "e22887d1e3e19cd2dba5",
+    "url": "/jobs-client/static/js/runtime-main.de3296e4.js"
+  },
+  {
+    "revision": "720a0ad751846037984edfc05fac5e3e",
+    "url": "/jobs-client/static/media/dot.720a0ad7.svg"
+  },
+  {
+    "revision": "488045302a5d96e2af50bd0f0b127918",
+    "url": "/jobs-client/static/media/exp.48804530.svg"
+  },
+  {
+    "revision": "0296bf0efdd634498dd086a26a89b633",
+    "url": "/jobs-client/static/media/fistbump-logo.0296bf0e.svg"
+  },
+  {
+    "revision": "eeee66c5c995bb17c5b7d28e24978115",
+    "url": "/jobs-client/static/media/interview.eeee66c5.svg"
+  },
+  {
+    "revision": "ceaa0781e98f9ec8d282b876ec98ef56",
+    "url": "/jobs-client/static/media/loader-white.ceaa0781.svg"
+  },
+  {
+    "revision": "3457399068b6d91d52c663eddbcb4a5e",
+    "url": "/jobs-client/static/media/loader.34573990.svg"
+  },
+  {
+    "revision": "9f46df7faeeaba3184744595564708a2",
+    "url": "/jobs-client/static/media/location.9f46df7f.svg"
+  },
+  {
+    "revision": "815c6cc3f80d7c0bdb266ea8ba1062a1",
+    "url": "/jobs-client/static/media/register.815c6cc3.svg"
+  },
+  {
+    "revision": "cb6077d6926f12757db71b86c0eed9c3",
+    "url": "/jobs-client/static/media/salary.cb6077d6.svg"
+  }
+]);
