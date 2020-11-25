08296ae987386503024f66c2d61af2998fecf987
fistbump-digital <74176421+fistbump-digital@users.noreply.github.com>
2020-11-26 00:23:46 +0530

    package json changes

---
+++ precache-manifest.649cc35533ca23925474ff9af674097b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcf5617a7cf711ad419b6786c84fb05",
+    "url": "/sos-candidates-dashboard/index.html"
+  },
+  {
+    "revision": "bb1a69b874690941347f",
+    "url": "/sos-candidates-dashboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c4eb325a2fd73ba4a85d",
+    "url": "/sos-candidates-dashboard/static/css/main.379300e0.chunk.css"
+  },
+  {
+    "revision": "bb1a69b874690941347f",
+    "url": "/sos-candidates-dashboard/static/js/2.04ed4db0.chunk.js"
+  },
+  {
+    "revision": "737a875f63054be206d1a30f820ffc03",
+    "url": "/sos-candidates-dashboard/static/js/2.04ed4db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4eb325a2fd73ba4a85d",
+    "url": "/sos-candidates-dashboard/static/js/main.f5840e8f.chunk.js"
+  },
+  {
+    "revision": "319a9029ce4c93b941eb",
+    "url": "/sos-candidates-dashboard/static/js/runtime-main.87e958a8.js"
+  },
+  {
+    "revision": "720a0ad751846037984edfc05fac5e3e",
+    "url": "/sos-candidates-dashboard/static/media/dot.720a0ad7.svg"
+  },
+  {
+    "revision": "488045302a5d96e2af50bd0f0b127918",
+    "url": "/sos-candidates-dashboard/static/media/exp.48804530.svg"
+  },
+  {
+    "revision": "0296bf0efdd634498dd086a26a89b633",
+    "url": "/sos-candidates-dashboard/static/media/fistbump-logo.0296bf0e.svg"
+  },
+  {
+    "revision": "eeee66c5c995bb17c5b7d28e24978115",
+    "url": "/sos-candidates-dashboard/static/media/interview.eeee66c5.svg"
+  },
+  {
+    "revision": "ceaa0781e98f9ec8d282b876ec98ef56",
+    "url": "/sos-candidates-dashboard/static/media/loader-white.ceaa0781.svg"
+  },
+  {
+    "revision": "3457399068b6d91d52c663eddbcb4a5e",
+    "url": "/sos-candidates-dashboard/static/media/loader.34573990.svg"
+  },
+  {
+    "revision": "9f46df7faeeaba3184744595564708a2",
+    "url": "/sos-candidates-dashboard/static/media/location.9f46df7f.svg"
+  },
+  {
+    "revision": "815c6cc3f80d7c0bdb266ea8ba1062a1",
+    "url": "/sos-candidates-dashboard/static/media/register.815c6cc3.svg"
+  },
+  {
+    "revision": "cb6077d6926f12757db71b86c0eed9c3",
+    "url": "/sos-candidates-dashboard/static/media/salary.cb6077d6.svg"
+  }
+]);
